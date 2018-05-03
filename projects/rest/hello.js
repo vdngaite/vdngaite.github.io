fb3cb4694c3bf5e00ea3d070459d5eaa5483af99
dsengmanivanh <daniel.sengmanivanh@pole-emploi.fr>
2018-05-03 20:49:58 +0200

    test api

--- projects/rest/hello.js
+++ projects/rest/hello.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
     $.ajax({
-        url: "http://rest-service.guides.spring.io/greeting"
+        url: "https://maintenant.pole-emploi.fr/v2/secteurs"
     }).then(function(data) {
-       $('.greeting-id').append(data.id);
-       $('.greeting-content').append(data.content);
+       $('.greeting-id').append(data.secteurs[0].id);
+       $('.greeting-content').append(data.secteurs[0].code);
     });
 });
