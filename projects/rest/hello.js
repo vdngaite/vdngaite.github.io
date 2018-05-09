b5d194888a8548903bab5e63cf3222a720dd361b
dsengmanivanh <daniel.sengmanivanh@pole-emploi.fr>
2018-05-09 15:40:38 +0200

    twine pdf

--- projects/rest/hello.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $.ajax({
-        url: "https://maintenant.pole-emploi.fr/v2/secteurs"
-    }).then(function(data) {
-       $('.greeting-id').append(data.secteurs[0].id);
-       $('.greeting-content').append(data.secteurs[0].code);
-    });
-});
